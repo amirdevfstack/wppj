dd30d657382437d0cb2e7a5ae081744cc1c92ef8
yesmrmoon <yesmrfake786@gmail.com>
2024-10-06 15:41:53 +0500

    slider

--- public/tnc-landing-page.php
+++ public/tnc-landing-page.php
@@ -310,9 +310,12 @@
  
    var swiperOne = new Swiper(".swiperSliderOne", {
              spaceBetween: 10,
-             mousewheel: true,
+             mousewheel: {
+               invert: false,
+               forceToAxis: true,
+               },
              keyboard: true,
-             cssMode:true,
+             cssMode:false,
              loop:true,
              speed:1000,
             //  autoplay: {
@@ -327,6 +330,7 @@
                nextEl: ".swiperSliderOneMain .swiper-button-next",
                prevEl: ".swiperSliderOneMain .swiper-button-prev",
             },
+
                breakpoints: {
                    320: {
                        slidesPerView: 2,
@@ -344,45 +348,47 @@
            });
    
            var swiperTwo = new Swiper(".swiperSliderTwo", {
-             spaceBetween: 10,
-             mousewheel: true,
-             keyboard: true,
-             cssMode:true,
-             loop:true,
-             speed:1000,
-            //  autoplay: {
-            //         delay: 2000,
-            //         disableOnInteraction: false
-            //     },
-             pagination: {
+               spaceBetween: 10,
+               mousewheel: {
+               invert: false,
+               forceToAxis: true,
+               },
+               keyboard: true,
+               cssMode: false,  // Disable cssMode
+               loop: true,
+               speed: 1000,
+               pagination: {
                el: ".swiperSliderTwo .swiper-pagination",
                clickable: true,
-             },
-             navigation: {
+               },
+               navigation: {
                nextEl: ".swiperSliderTwo .swiper-button-next",
                prevEl: ".swiperSliderTwo .swiper-button-prev",
-            },
-               breakpoints: {
-                   320: {
-                       slidesPerView: 2,
-                   },
-                   400: {
-                       slidesPerView: 2,
-                   },
-                   768: {
-                       slidesPerView: 4,
-                   },
-                   1200: {
-                       slidesPerView: 6,
-                   },
                },
-           });
-   
+               breakpoints: {
+               320: {
+                  slidesPerView: 2,
+               },
+               400: {
+                  slidesPerView: 2,
+               },
+               768: {
+                  slidesPerView: 4,
+               },
+               1200: {
+                  slidesPerView: 6,
+               },
+               },
+            });
+
            var speakerSlider = new Swiper(".speakerSlider", {
              spaceBetween: 10,
-             mousewheel: true,
+             mousewheel: {
+               invert: false,
+               forceToAxis: true,
+               },
              keyboard: true,
-             cssMode:true,
+             cssMode:false,
              slidesPerView: 4,
              loop:true,
              speed:1000,
@@ -390,6 +396,7 @@
                el: ".speakerSlider .swiper-pagination",
                clickable: true,
              },
+          
              navigation: {
                nextEl: ".speakerSlider .swiper-button-next",
                prevEl: ".speakerSlider .swiper-button-prev",
